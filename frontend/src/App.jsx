69ad75a21d1b9feab954d16c7d46859a99990839
sumesh <sumeshmandlik2018@gmail.com>
2023-02-23 16:00:10 +0530

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import { Sidebar, Navbar } from './components';
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
 const App = () => {
-    return(
-        <div>
-            basic setup
-            temp branch
-        </div>
-        
-    )
+  return (
+    <div className="relative sm:-8 p-4 bg-[#c2044d] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes>
+      </div>
+    </div>
+  )
 }
 
 export default App
