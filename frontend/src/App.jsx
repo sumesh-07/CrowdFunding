0b38cfd2bf988cfdfe0f0772e8d870eb6ed35048
sumesh <sumeshmandlik2018@gmail.com>
2023-02-23 13:42:13 +0530

    basic_App_setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom';
+
+const App = () => {
+    return(
+        <div>
+            basic setup
+        </div>
+        
+    )
+}
+
+export default App
